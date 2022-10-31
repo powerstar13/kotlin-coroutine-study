6f371fa43b9f95503c57c952f02e98026cdecee3
홍준성 <ghdwnstjd13@gmail.com>
2022-10-31 17:41:45 +0900

    New Project > git init

---
+++ src/main/kotlin/study/coroutine/example/KotlinCoroutineStudyApplication.kt
@@ -0,0 +1,11 @@
+package study.coroutine.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCoroutineStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCoroutineStudyApplication>(*args)
+}
---
+++ src/test/kotlin/study/coroutine/example/KotlinCoroutineStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.coroutine.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinCoroutineStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
